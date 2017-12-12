180211dcb580e8d6dd1e59c6602eb5b0e4d7a7d0
inuitwallet <contact@inuit-wallet.co.uk>
2017-12-12 13:38:49 +0000

    add new dns seed and checkpoints up to 1500000

--- core/src/main/java/com/matthewmitchell/nubitsj/params/MainNetParams.java
+++ core/src/main/java/com/matthewmitchell/nubitsj/params/MainNetParams.java
@@ -55,10 +55,16 @@ public class MainNetParams extends AbstractNubitsNetParams {
         checkpoints.put(120320, new Sha256Hash("d6ed61ffe1ba2b63fbbe54b8aab43763467b406fa9d0374ede4d8397d880ffaf"));
         checkpoints.put(160000, new Sha256Hash("da14f6ce38bddb33af0f2f448a8d3f7b99b16157a06db7a3706b8d37bb9d2b42"));
         checkpoints.put(200000, new Sha256Hash("1fa6966628f46a08cbfd1cb0422bdaa67003c8f0b8e141a8e00f7fc1e9354d59"));
+        checkpoints.put(300000, new Sha256Hash("9b9620d1a08cdb6050a0e8fae598c1cf7a33d141598075462fb7614b07000c77"));
+        checkpoints.put(500000, new Sha256Hash("03eddb9d6d13fbe1053236f2e2b9986c0d621d0457f7454b94950b5c74817a4d"));
+        checkpoints.put(1000000, new Sha256Hash("a29c7bb67686fa6b5a643fd76198535f38d313b0a59834834a8ce049aba835f7"));
+        checkpoints.put(1500000, new Sha256Hash("c91a722860758eb09c47ded633880aaf10c5c4508aa32acb4de6505a98f362b4"));
+        
 
         dnsSeeds = new String[] {
             "nuseed.coinerella.com",
-            "nuseed.nubitsexplorer.nu"
+            "nuseed.nubitsexplorer.nu",
+            "seed.nu.crypto-daio.co.uk",
         };
 
     }
